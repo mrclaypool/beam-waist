9ff03372adeb0b5ab4c7be7f2a072049d5eb816f
Miranda Claypool <143661946+mrclaypool@users.noreply.github.com>
2024-07-31 16:22:04 -0700

    Updated bwf2.py
    Made the graphs look nicer, added a few little features like graphing just the intensity for each column and row for each image, and a way to convert to physical quantities.

--- bwf2.py
+++ bwf2.py
@@ -7,23 +7,35 @@ Created on Thu Feb 22 11:14:57 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scienceplots
 from PIL import Image#, ImageFilter
 from scipy.optimize import curve_fit
-
+#plt.style.use('science')
 
 #%%functions for fitting
 def gaussian(r,a,r0,sigma,n):
-    return a*np.exp(-((r-r0)/(2*sigma))**2)
+    return a*np.exp(-((r-r0)/(2*sigma))**2) #to fit a super gaussian change 2 to 4,6,8 etc
 #%%looping through images and making arrays of the max values of each column and row
-count=106
-folder='7-9-24b'
-radiic=np.zeros(count)
+count=108
+folder='7-31-24-3i' #folder that contains images relative to the location of this file
+radiic=np.zeros(count) #radii for a circle fit
 radiixc=np.zeros(count)
 radiiyc=np.zeros(count)
 
-radiig=np.zeros(count)
+radiig=np.zeros(count) #radii arrays for a gaussian fit
 radiixg=np.zeros(count)
 radiiyg=np.zeros(count)
+
+pixeltomm=281/(25.4)
+
+#%%luminosity conversion from Mary A Pagnutti, Ryan, Cazanavette et al, SPIE 2017
+#these values are taken from the metadata for the images. Change if using a different camera
+def lum(DN):
+    C=0.522 #for red channel from article
+    fnum=1.2 #from arducam wide angle 6 mm lens 
+    tau=0.10 #exposure time
+    ISO=160 #from photo metadata
+    return C*(fnum)**2/tau/ISO*DN
 
 #%% Plotting for a circle
 # for k in range(0,count):
@@ -104,11 +116,13 @@ for k in range(0,count):
     iimg=Image.open(folder+'/'+string+'.jpg')
     # blurimg=iimg.filter(ImageFilter.BoxBlur(9))
     timg=np.array(iimg)[:,:,0] #only red channel
-
+    ogtimg=timg.copy()
+    #timg=lum(timg) #convert ot luminosity or intensity units
+    
     avg=np.average(timg)
-    timg=timg-0.8*avg
+    timg=timg-avg
     #print('avg', avg)
-    xc1,xc2,yc1,yc2= 550,1200, 900,1600
+    xc1,xc2,yc1,yc2= 550,1300, 800,1600
     timg=timg[xc1:xc2,yc1:yc2]
     
     ylim,xlim=np.shape(timg)
@@ -123,64 +137,68 @@ for k in range(0,count):
     for i,j in enumerate(ymax): #making an array of the max value of every row
         #print(i,j,np.max(timg[i]))
         ymax[i]=np.max(timg[i])
-        #print(ymax)    
-    
+        #print(ymax)  
+        
+    ###################  
     #using all points
-    # xfit=xmax
-    # yfit=ymax
-    # xcoordsfit=xcoords=np.arange(0,xlim)
-    # ycoordsfit=ycoords=np.arange(0,ylim)
+    xfit=xmax
+    yfit=ymax
+    xcoordsfit=xcoords=np.arange(0,xlim)#/pixeltomm
+    ycoordsfit=ycoords=np.arange(0,ylim)#/pixeltomm
+    ###################
 
+    ###################
     #only looking at wings by excluding the center
     #picking out values that arent saturated for x    
-    for i,j in enumerate(xmax):
-        if j<np.max(xmax):
-            continue
-        else:
-            satst=i
-            break
+    # for i,j in enumerate(xmax):
+    #     if j<np.max(xmax):
+    #         continue
+    #     else:
+    #         satst=i
+    #         break
     
-    xmaxrev=xmax[::-1]
-    for i,j in enumerate(xmaxrev):
-        if j<np.max(xmax):
-            continue
-        else:
-            satend=len(xmax)-i
-            break
+    # xmaxrev=xmax[::-1]
+    # for i,j in enumerate(xmaxrev):
+    #     if j<np.max(xmax):
+    #         continue
+    #     else:
+    #         satend=len(xmax)-i
+    #         break
         
-    xfit1=xmax[:satst]
-    xfit2=xmax[satend:]
-    xfit=np.append(xfit1,xfit2)
+    # xfit1=xmax[:satst]
+    # xfit2=xmax[satend:]
+    # xfit=np.append(xfit1,xfit2)
     
-    xcoords=np.arange(0,xlim)    
-    xcoordsfit1=xcoords[:satst]
-    xcoordsfit2=xcoords[satend:]
-    xcoordsfit=np.append(xcoordsfit1,xcoordsfit2)
+    # xcoords=np.arange(0,xlim)    
+    # xcoordsfit1=xcoords[:satst]
+    # xcoordsfit2=xcoords[satend:]
+    # xcoordsfit=np.append(xcoordsfit1,xcoordsfit2)
     
-    #now for y
-    for i,j in enumerate(ymax):
-        if j<np.max(ymax):
-            continue
-        else:
-            satsty=i
-            break
+    # #now for y
+    # for i,j in enumerate(ymax):
+    #     if j<np.max(ymax):
+    #         continue
+    #     else:
+    #         satsty=i
+    #         break
     
-    ymaxrev=ymax[::-1]
-    for i,j in enumerate(ymaxrev):
-        if j<np.max(ymax):
-            continue
-        else:
-            satendy=len(ymax)-i
-            break
+    # ymaxrev=ymax[::-1]
+    # for i,j in enumerate(ymaxrev):
+    #     if j<np.max(ymax):
+    #         continue
+    #     else:
+    #         satendy=len(ymax)-i
+    #         break
         
-    yfit1=ymax[:satsty]
-    yfit2=ymax[satendy:]
-    yfit=np.append(yfit1,yfit2)
+    # yfit1=ymax[:satsty]
+    # yfit2=ymax[satendy:]
+    # yfit=np.append(yfit1,yfit2)
     
-    ycoords=np.arange(0,ylim)
-    ycoordsfit1=ycoords[:satsty]
-    ycoordsfit2=ycoords[satendy:]
-    ycoordsfit=np.append(ycoordsfit1,ycoordsfit2)
+    # ycoords=np.arange(0,ylim)
+    # ycoordsfit1=ycoords[:satsty]
+    # ycoordsfit2=ycoords[satendy:]
+    # ycoordsfit=np.append(ycoordsfit1,ycoordsfit2)
+    ###########
     
     #fitting/plotting for a gaussian           
     paramx, pcovx = curve_fit(gaussian, xcoordsfit,xfit)
@@ -196,22 +214,48 @@ for k in range(0,count):
     fwhmx=2*sigmax**2 #should this have a parenthese? (2*sigmax)**2?
     fwhmy=2*sigmay**2
     
-    #radiik=(np.sqrt(fwhmx+fwhmy))/2
-    #radiig[k]=radiik
+    radiik=(np.sqrt(fwhmx+fwhmy))/2
+    radiig[k]=radiik
+    #plt.style.use(["science",'ieee','bright'])
+    plt.style.use('default')
     radiixg[k]=np.sqrt(fwhmx)
     radiiyg[k]=np.sqrt(fwhmy)
     
     plt.imshow(timg)
 
-    plt.plot(xcoords,xmax)
-    plt.plot(xcoordsfit,gaussian(xcoordsfit,paramx[0],paramx[1],paramx[2],paramx[3]))
+    plt.plot(xcoords,-xmax+(xc2-xc1), label = 'Max Column Red DN')
+    plt.plot(xcoordsfit,-gaussian(xcoordsfit,paramx[0],paramx[1],paramx[2],paramx[3])+(xc2-xc1)-10, label = 'Gaussian Fit in x', linestyle='dashed')
 
-    plt.plot(ymax,ycoords)
-    plt.plot(gaussian(ycoordsfit,paramy[0],paramy[1],paramy[2],paramy[3]),ycoordsfit)
+    plt.plot(ymax,ycoords,label = 'Max Row Red DN')
+    plt.plot(gaussian(ycoordsfit,paramy[0],paramy[1],paramy[2],paramy[3]),ycoordsfit, label = 'Gaussian Fit in y', linestyle='dashed')
     
+    plt.xlabel('X Pixel Coordinate')
+    plt.ylabel('Y Pixel Coordinate')
+    leg = plt.legend()
+    
+    for text in leg.get_texts():
+        text.set_color("white")
     plt.title(folder+' '+string)
+    #plt.title('Cropped Image')
     plt.show()
-
+    
+    #this section lets you plot just the intensity and gaussian for x, y, and r
+    # plt.style.use(["science",'ieee'])
+    # #to plot separately
+    # plt.plot(xcoords,xmax, label='Data')
+    # plt.plot(xcoordsfit,gaussian(xcoordsfit,paramx[0],paramx[1],paramx[2],paramx[3]), label='Gaussian Fit')
+    # plt.legend()
+    # plt.xlabel('Coordinate of columns in image (mm)')
+    # plt.ylabel('Intensity in red channel')
+    # plt.show()
+    
+    # plt.plot(ycoords,ymax, label='Data')
+    # plt.plot(ycoordsfit,gaussian(ycoordsfit,paramy[0],paramy[1],paramy[2],paramy[3]), label='Gaussian Fit')
+    # plt.legend()
+    # plt.xlabel('Coordinate of rows in image (mm)')
+    # plt.ylabel('Intensity in red channel')
+    # plt.show()
+    
 #%%graphing parameters for radius vs z
 
 photonum=np.arange(0,count) #numbering all of the photos (will tell us how many steps)
@@ -220,7 +264,7 @@ radmirr=125.12/2 #for the new big mirror
 #dfrommirr=1.182*10 + 2.54*10 #added one in bc i moved setup
 #dfrommirr=7.4*10 #for the big mirror
 #dfrommirr=12.75*10 #new big mirror
-dfrommirr=100.09 -1.77#new setup for big mirror
+dfrommirr=100.09 -1.77#+25.4#new setup for big mirror
 drail=8.7*10
 dperstep=drail/(count) #how many mm per step
 
@@ -239,14 +283,15 @@ def rayleigh(z,n,w0,z0,lam):
 def line(x,m,b):
     y=m*x+b
     return y
+
 #%%graphing radii vs z to get focal point:
 #radiic=np.sqrt(2*radiixc**2+2*radiiyc**2)/2
 radiig=np.sqrt(2*radiixg**2+2*radiiyg**2)/2
 
 #radiiarr=radiixc,radiiyc,radiic,radiixg,radiiyg,radiig
 radiiarr=radiixg,radiiyg,radiig
-labels=['x gaus, ', 'y gaus, ', 'radii gaus, ']
-font1 = {'family':'serif','size':28}
+labels=['x gaus, ', 'y gaus, ', '']
+#font1 = {'family':'serif','size':28}
 
 for j, i in enumerate(radiiarr):
     
@@ -257,8 +302,6 @@ for j, i in enumerate(radiiarr):
     thres=20
     r_D=np.abs(np.diff(radii1,axis=0))
     r_bad=np.where(r_D>thres) #we expect points to smoothly transition into each other
-    #for i in r_bad[0]:
-    #    radii1[i]=np.average(radii1[i-5:i+5])
     
     mask=np.ones(len(radii1), dtype=bool)
     mask[[r_bad]]=False
@@ -330,33 +373,44 @@ for j, i in enumerate(radiiarr):
     # print(imprfit,thresh)
     # print(np.where(imprfit>thresh))
     # print(np.mean(imprfit))
-
-    plt.rcParams["font.family"] = "Times New Roman"
-    plt.figure(figsize=(8,5),dpi=300)
-    plt.scatter(x2,radiicorr, color='coral')
-    plt.scatter(x2,-radiicorr, color='coral')
+    
+    #make it look nice
+    # plt.rcParams["font.family"] = "Times New Roman"
+    # plt.figure(figsize=(8,5),dpi=300)
+    plt.style.use(['science','ieee'])
+    plt.figure()#figsize=(8,5),dpi=300)
+    
+    #plot radii v position mirrored to show beam width
+    plt.scatter(x2,radiicorr, color='red', s=5)
+    plt.scatter(x2,-radiicorr, color='red', s=5)
     plt.axhline(y = 0.0, color = 'grey', linestyle = 'dashed')
  
-    #plt.plot(focpoint2,beamwidth2, marker='*',color='blue',markersize=18,label='Rayleigh fit: '+str(np.round(focpoint2[0],3))+' mm')
-    plt.plot(focpoint2,beamwidth2, marker='*',color='blue',markersize=18,label='Rayleigh fit: '+str(np.round(focpoint2,3))+' mm')
-    plt.plot(focpoint2, -beamwidth2,marker='*',color='blue',markersize=18)
-
-    # plt.plot(x0,0,marker='*',color='red',markersize=15,label='Line fit: '+str(x0)+' mm')
+    #plot the focal point gained from rayleigh fit
+    #plt.plot(focpoint2,beamwidth2, marker='*',color='blue',markersize=18,label='Rayleigh fit: '+str(np.round(focpoint2,3))+' mm')
+    #plt.plot(focpoint2, -beamwidth2,marker='*',color='blue',markersize=18)
     
-    plt.plot(x2,rayleigh(x2,*fit),color='blue')
-    plt.plot(x2,-rayleigh(x2,*fit),color='blue')
-
+    plt.plot(focpoint2,beamwidth2, marker='*',color='blue',label='Focal length: '+str(np.round(focpoint2,3))+' mm')
+    plt.plot(focpoint2, -beamwidth2,marker='*',color='blue')
+    #plot rayleigh fit
+    plt.plot(x2,rayleigh(x2,*fit), linestyle='dashed', color='blue', )#,color='blue')
+    plt.plot(x2,-rayleigh(x2,*fit), linestyle='dashed', color='blue', label="Beam Width Fit")#,color='blue')
+    
+    #plot the focal point from the line fit
+    #plt.plot(x0,0,marker='*',color='red',markersize=15,label='Line fit: '+str(x0)+' mm')
+    
+    #plot the line fit 
     # plt.plot(x2,line(x2,*linefit),color='blue')
     # plt.plot(x2,-line(x2,*linefit),color='blue')
-
+    
+    #shows what points are being used by linefit
     # plt.plot(x2[maskr],rayleigh(x2,*fit)[maskr],color='red')
     # plt.plot(x2[maskr],-rayleigh(x2,*fit)[maskr],color='red')
     
-    plt.xlabel(labels[j]+'Distance from Mirror (mm)', fontdict=font1)
+    plt.xlabel(labels[j]+'Distance from Mirror (mm)')#, fontdict=font1)
 
-    plt.ylabel('Width of Beam (mm)', fontdict=font1)
-    plt.tick_params(labelsize='20')
+    plt.ylabel('Width of Beam (mm)')#, fontdict=font1)
+    plt.tick_params()
     
-    plt.legend(fontsize=20,loc='upper left')
+    plt.legend(loc='right')
     plt.show()
 
